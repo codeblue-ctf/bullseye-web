94d1a046ec563059632223d604b39deccb848fc5
tyage <namatyage@gmail.com>
2018-08-05 19:35:00 +0900

    rails g scaffold problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_160341) do
+ActiveRecord::Schema.define(version: 2018_08_05_103426) do
+
+  create_table "problems", force: :cascade do |t|
+    t.string "title"
+    t.text "statement"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "email", default: "", null: false
