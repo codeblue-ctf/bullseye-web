e1e505e753ecbd5eb5c59b802a38438421410309
tyage <namatyage@gmail.com>
2018-10-02 23:33:39 +0900

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :problems
+  resources :problems, only: [:index, :show]
   resource :auth
-  devise_for :teams
+  devise_for :teams, only: [:sign_in, :sign_out]
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
