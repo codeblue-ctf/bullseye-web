cd3640ad27861d53c6118a3fd0c886364af2c411
tyage <namatyage@gmail.com>
2018-10-06 21:25:42 +0900

    show real instruction of submitting exploit

--- config/initializers/bullseye_parameters.rb
+++ config/initializers/bullseye_parameters.rb
@@ -1,7 +1,5 @@
-RailsAdmin.config do |config|
-  config.bullseye = {
-    bullseye_runner_host: ENV.fetch('BULLSEYE_RUNNER_HOST') { 'localhost:3001' },
-    docker_registry_host: ENV.fetch('DOCKER_REGISTRY_HOST') { 'localhost:3002' },
-    docker_registry_auth_host: ENV.fetch('DOCKER_REGISTRY_AUTH_HOST') { 'localhost:3003' }
-  }
-end
+Rails.application.config.bullseye = {
+  bullseye_runner_host: ENV.fetch('BULLSEYE_RUNNER_HOST') { 'localhost:3001' },
+  docker_registry_host: ENV.fetch('DOCKER_REGISTRY_HOST') { 'localhost:3002' },
+  docker_registry_auth_host: ENV.fetch('DOCKER_REGISTRY_AUTH_HOST') { 'localhost:3003' }
+}
