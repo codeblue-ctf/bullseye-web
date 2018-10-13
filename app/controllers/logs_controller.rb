64ba5baa3dee6542dd4a2ffe2f24dcf0d6c921cd
tyage <namatyage@gmail.com>
2018-10-14 03:43:47 +0900

    show submit log

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,10 +3,9 @@ require './lib/docker_registry'
 class LogsController < ApplicationController
   before_action :authenticate_team!
   def submit_log
-    problems = Problem.all
-    @infos = problems.map do |problem|
+    @problems = Problem.all
+    @infos = @problems.map do |problem|
       DockerRegistry::get_latest_info(current_team.name, problem.exploit_container_name)
     end
-    render json: { result: @infos }
   end
 end
