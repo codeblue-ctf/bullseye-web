dc291b0d22fe0fda1008e6cda5517a7890fdbb7a
tyage <namatyage@gmail.com>
2018-10-14 00:22:45 +0900

    get jwt from docker registry auth server

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,12 +1,11 @@
-require '../../lib/docker_registry'
+require './lib/docker_registry'
 
 class LogsController < ApplicationController
   before_action :authenticate_team!
   def submit_log
     problems = Problem.all
     @infos = problems.map do |problem|
-      DockerRegistry::get_latest_info(current_user.name, problem.name)
+      DockerRegistry::get_latest_info(current_team.name, problem.exploit_container_name)
     end
   end
 end
-
--- lib/docker_registry.rb
+++ lib/docker_registry.rb
@@ -1,10 +1,22 @@
+require 'net/https'
+require 'uri'
+
 module DockerRegistry
-  def login(team_name, problem_name)
-    curl -u "<teamId>:<teamToekn>" "https://<DOCKER_REGISTRY_AUTH>/auth?account=<teamId>&scope=repository:binja/hello-world:pull&service=Docker%20registry" -k
+  def self.login(team_name, problem_name)
+    bullseye_config = Rails.application.config.bullseye
+    uri = URI.parse("https://#{bullseye_config[:docker_registry_auth_host]}/auth?account=#{bullseye_config[:admin][:name]}&scope=repository:#{team_name}/#{problem_name}:pull&service=Docker%20registry")
+    res = Net::HTTP.start(uri.host, uri.port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) { |http|
+      req = Net::HTTP::Get.new(uri)
+      req.basic_auth(bullseye_config[:admin][:name], bullseye_config[:admin][:password])
+      http.request(req)
+    }
+    body = JSON.parse(res.body)
+    body['token']
   end
 
-  def get_latest_info(team_name, problem_name)
+  def self.get_latest_info(team_name, problem_name)
     token = login(team_name, problem_name)
-    curl http://<DOCKER_REGISTRY>/v2/binja/hello-world/manifests/latest -H "Authorization: Bearer <token>"
+    print(token)
+    # curl http://<DOCKER_REGISTRY>/v2/binja/hello-world/manifests/latest -H "Authorization: Bearer <token>"
   end
 end
