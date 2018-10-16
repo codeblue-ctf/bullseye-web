043d66bfdd78b8a3b205d3b2b418d3c00a7fb80e
tyage <namatyage@gmail.com>
2018-10-16 23:31:28 +0900

    add result receiver controller

---
+++ app/controllers/result_receiver_controller.rb
@@ -0,0 +1,20 @@
+class ProblemsController < ApplicationController
+  before_action :authenticate_team!
+  before_action :set_problem, only: [:show]
+
+  # GET /problems
+  # GET /problems.json
+  def index
+    @problems = Problem.all
+  end
+
+  # GET /problems/1
+  # GET /problems/1.json
+  def show
+  end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_problem
+      @problem = Problem.find(params[:id])
+    end
+end
