728b72b8405b52c2a9d22a8933243569ae4b6143
icchy <icchyr@gmail.com>
2018-10-18 20:58:04 +0900

    fix interval for sidekiq (=> every 1 second)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379', namespace: 'sidekiq' }
+  config.average_scheduled_poll_interval = 1
 end
 
 Sidekiq.configure_client do |config|
