77195a7b93bcdf8815301443f605b276eafd84a7
tyage <namatyage@gmail.com>
2018-10-20 16:41:49 +0900

    register start_at and finished_at

--- app/controllers/api/v1/schedule_results_controller.rb
+++ app/controllers/api/v1/schedule_results_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::ScheduleResultsController < ApiController
   def submit
     # Register score
-    ScheduleResult.create(
-      schedule_id: params[:schedule_id],
-      score: params[:score],
-      error: params[:error]
-    )
+    ScheduleResult.create(schedule_result_params)
+  end
+
+  private
+  def schedule_result_params
+    params.require(:schedule_result).permit(:schedule_id, :score, :error, :start_at, :finished_at)
   end
 end
