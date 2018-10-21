715d26c98de7ba55d3491ae42f62a3626098b50e
tyage <namatyage@gmail.com>
2018-10-21 17:18:27 +0900

    create seed data if not exists

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,14 +1,18 @@
-Admin.create(
-  email: 'admin@localhost',
-  password: 'password'
-)
-Team.create(
-  email: 'team1@localhost',
-  password: 'password',
-  name: 'team1',
-  docker_registry_token: 'token'
-)
-Problem.create(
+Admin.find_or_create_by(email: 'admin@localhost') do |admin|
+  admin.update(
+    password: 'password'
+  )
+end
+
+Team.find_or_create_by(email: 'team1@localhost') do |team|
+  team.update(
+    password: 'password',
+    name: 'team1',
+    docker_registry_token: 'token'
+  )
+end
+
+Problem.find_or_create_by(
   title: 'sanity-check',
   statement: 'sanity-check'
 )
