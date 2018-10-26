db053e542b62c5042515b4e70ca7f2f97b389601
tyage <namatyage@gmail.com>
2018-10-26 23:47:42 +0900

    fix config

--- config/initializers/bullseye_parameters.rb
+++ config/initializers/bullseye_parameters.rb
@@ -1,5 +1,7 @@
+runner_host = ENV.fetch('BULLSEYE_RUNNER_HOST') { 'localhost:3001' },
+
 Rails.application.config.bullseye = {
-  runner_host: ENV.fetch('BULLSEYE_RUNNER_HOST') { 'localhost:3001' },
+  runner_host: runner_host
   docker_registry_host: ENV.fetch('DOCKER_REGISTRY_HOST') { 'localhost:5000' },
   docker_registry_auth_host: ENV.fetch('DOCKER_REGISTRY_AUTH_HOST') { 'localhost:5001' },
   api_authorization_token: ENV.fetch('BULLSEYE_API_AUTH_TOKEN') { 'please set auth token' },
@@ -7,5 +9,5 @@ Rails.application.config.bullseye = {
     name: ENV.fetch('BULLSEYE_ADMIN_NAME') { 'admin' },
     password: ENV.fetch('BULLSEYE_ADMIN_PASSWORD') { 'password' }
   },
-  runner_start_endpoint: "#{Rails.application.config.bullseye[:runner_host]}/"
+  runner_start_endpoint: "#{runner_host}/"
 }
