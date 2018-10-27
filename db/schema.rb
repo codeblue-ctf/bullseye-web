c500ed735f021dcc51420baa1144a9901c2358b8
tyage <namatyage@gmail.com>
2018-10-28 00:54:46 +0900

    add unique constraint to login name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_155111) do
+ActiveRecord::Schema.define(version: 2018_10_27_155337) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2018_10_27_155111) do
     t.string "name"
     t.string "login_name"
     t.index ["email"], name: "index_teams_on_email", unique: true
+    t.index ["login_name"], name: "index_teams_on_login_name", unique: true
     t.index ["reset_password_token"], name: "index_teams_on_reset_password_token", unique: true
   end
 
