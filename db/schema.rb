ed0c29386e797ddb2f90e7ffca4ad0fe8cb51929
tyage <namatyage@gmail.com>
2018-10-28 01:19:02 +0900

    add login name to admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_155337) do
+ActiveRecord::Schema.define(version: 2018_10_27_161756) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2018_10_27_155337) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "login_name"
     t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["login_name"], name: "index_admins_on_login_name", unique: true
   end
 
   create_table "problems", force: :cascade do |t|
