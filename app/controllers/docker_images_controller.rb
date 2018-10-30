3170d6b8e54308401e2e8932aa86892a95677d1c
tyage <namatyage@gmail.com>
2018-10-30 20:40:16 +0900

    add show all images action

---
+++ app/controllers/docker_images_controller.rb
@@ -0,0 +1,21 @@
+require './lib/docker_registry'
+
+class DockerImagesController < ApplicationController
+  before_action :authenticate_team!, only: [:my]
+  before_action :authenticate_admin!, only: [:all]
+
+  def my
+    @problems = Problem.all
+    @infos = @problems.map do |problem|
+      DockerRegistry::get_latest_info(current_team.name, problem.exploit_container_name)
+    end
+  end
+
+  def all
+    @teams = Teams.all
+    @problems = Problem.all
+    @infos = @problems.map do |problem|
+      DockerRegistry::get_latest_info(current_team.name, problem.exploit_container_name)
+    end
+  end
+end
