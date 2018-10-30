4ab4d7b0cae3ba0c083bc3e8b5079dfe49fe5187
tyage <namatyage@gmail.com>
2018-10-30 19:42:26 +0900

    remove default value from text field

--- db/migrate/20181030084152_add_score_to_schedule_results.rb
+++ db/migrate/20181030084152_add_score_to_schedule_results.rb
@@ -3,7 +3,7 @@ class AddScoreToScheduleResults < ActiveRecord::Migration[5.2]
     add_column :schedule_results, :score, :integer
 
     add_column :schedule_results, :failed, :integer
-    add_column :problems, :calc_formula, :text, default: "# available variables: succeeded, failed, started_at, finished_at\n1000*succeeded/(succeeded + failed)", null: false
+    add_column :problems, :calc_formula, :text, null: false
 
     rename_column :schedule_results, :start_at, :runner_started_at
     rename_column :schedule_results, :finished_at, :runner_finished_at
