8408c4c337249fb7a636aa1465c88648d1e78565
tyage <namatyage@gmail.com>
2019-09-28 23:34:57 +0900

    create schedules for each times

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,10 +7,12 @@ class SchedulesController < ApplicationController
   end
 
   def create_macro
-    Schedule.create(
-      teams: params[:teams].to_json,
-      problems: params[:problems].to_json,
-      start_times: params[:start_times].to_json
-    )
+    params[:start_times].each do |start_time|
+      Schedule.create(
+        teams: params[:teams].to_json,
+        problems: params[:problems].to_json,
+        start_at: start_time
+      )
+    end
   end
 end
