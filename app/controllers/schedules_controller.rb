00efed599945df3037d518c1f70efc8fea4a1102
tyage <namatyage@gmail.com>
2019-10-20 19:18:51 +0900

    use team login id and exploit container name instead of team id and problem id

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -6,14 +6,21 @@ class SchedulesController < ApplicationController
   end
 
   def create_macro
-    params[:teams].each do |team|
-      Problem.where(id: params[:problems]).each do |problem|
-        yml = DockerComposeTemplate::format_to_runner_template(problem.docker_compose, team: team)
+    teams = Team.where(id: params[:teams])
+    problems = Problem.where(id: params[:problems])
+    teams.each do |team|
+      problems.each do |problem|
+        # format docker compose template to runner's one
+        yml = DockerComposeTemplate::format_to_runner_template(
+          problem.docker_compose,
+          team: team.login_name
+        )
+
         RunnerMaster::create_schedule(
-          team_id: team.to_s,
-          problem_id: problem.id.to_s,
+          team_id: team.login_name,
+          problem_id: problem.exploit_container_name,
           start_at: Time.parse(params[:start_at]).iso8601, # TODO: get time from round id
-          stop_at: Time.parse(params[:stop_at]).iso8601,
+          stop_at: Time.parse(params[:stop_at]).iso8601, # TODO: this should be JST
           yml: yml,
           interval: params[:interval].to_i,
           ntrials: problem.ntrials,
