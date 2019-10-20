3c88089688950410993c53f9f29c1888ff6ac006
tyage <namatyage@gmail.com>
2019-10-20 17:23:36 +0900

    make interval to int

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -14,7 +14,7 @@ class SchedulesController < ApplicationController
           start_at: Time.parse(params[:start_at]).iso8601, # TODO: get time from round id
           stop_at: Time.parse(params[:stop_at]).iso8601,
           yml: problem.docker_compose,
-          interval: params[:interval],
+          interval: params[:interval].to_i,
           ntrials: problem.ntrials,
           timeout: problem.timeout,
           worker_hosts: params[:worker_hosts],
