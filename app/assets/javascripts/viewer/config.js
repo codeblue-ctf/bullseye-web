23a12bd822c8c10eb792a3639c8363f76322d68b
mage <mage.noread.noreply@gmail.com>
2019-10-27 16:49:10 +0000

    delete magedev

--- app/assets/javascripts/viewer/config.js
+++ app/assets/javascripts/viewer/config.js
@@ -1,4 +1,3 @@
 const CONFIG = {}
 CONFIG.OBJECTS_NUMBER = 100
-//CONFIG.API_BASE = 'http://a048dec177a96b41d28697fa.codeblue.ohcihsiek.jp/external_api/v1/viewer'
-CONFIG.API_BASE = 'http://e72e7b2cfe3d888d7a03f52f.codeblue.ohcihsiek.jp/external_api/v1/viewer'
+CONFIG.API_BASE = '/external_api/v1/viewer'
--- app/javascript/viewer/components/Play.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h2>play</h2>
-</template>
-
-<script>
-export default {
-}
-</script>
