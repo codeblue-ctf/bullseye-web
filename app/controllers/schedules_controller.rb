16a665507247cff534e274fe78abbf9ff32f80d4
tyage <namatyage@gmail.com>
2019-10-28 01:35:39 +0900

    rename problem -> exploit_container

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -32,7 +32,7 @@ class SchedulesController < ApplicationController
           registry_password: bullseye_config[:admin][:password],
           callback_url: '',
           team: team.login_name,
-          problem: problem.exploit_container_name,
+          exploit_container: problem.exploit_container_name,
           x11_required: problem.x11_required,
           flag_template: problem.flag_template,
         )
