3601aa56862cf476e9e826f614913288243c88f9
mage <mage.noread.noreply@gmail.com>
2019-10-28 07:24:52 +0000

    Add capture api

--- app/assets/javascripts/viewer/config.js
+++ app/assets/javascripts/viewer/config.js
@@ -1,4 +1,11 @@
 const CONFIG = {}
 CONFIG.OBJECTS_NUMBER = 100
-CONFIG.API_BASE = '/external_api/v1/viewer'
-//CONFIG.API_BASE = 'http://e72e7b2cfe3d888d7a03f52f.codeblue.ohcihsiek.jp/external_api/v1/viewer'
+
+switch (location.origin) {
+    case 'https://bullseye.ctf.codeblue.jp':
+        CONFIG.API_BASE = '/external_api/v1/viewer'
+        break
+    default:
+        CONFIG.API_BASE = 'http://e72e7b2cfe3d888d7a03f52f.codeblue.ohcihsiek.jp/external_api/v1/viewer'
+        break
+}
--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -9,4 +9,9 @@ class ViewerController < ApplicationController
 
   def table
   end
+
+  def capture
+    job_capture = RunnerMaster::get_job_capture(params[:id])
+    send_data(job_capture, :disposition => 'inline', :type => 'video/mp4')
+  end
 end
