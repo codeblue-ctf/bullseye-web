c7f23183f778679461c6c3637e06b35797f5def9
tyage <namatyage@gmail.com>
2019-10-29 00:36:58 +0900

    players' manual

--- app/javascript/components/Home.vue
+++ app/javascript/components/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="display-4 mb-4">CODE BLUE CTF 2019</h1>
 
-    <p>Welcome to the CTF. Please read the docs you received.</p>
+    <p>Welcome to the CTF. Please read players' manual.</p>
   </div>
 </template>
 
