3cbe9f597e117efb8d72684e4bd6254c54caaf63
mage <mage.noread.noreply@gmail.com>
2019-10-29 23:40:24 +0000

    update viewer

--- app/assets/javascripts/viewer/config.js
+++ app/assets/javascripts/viewer/config.js
@@ -2,11 +2,14 @@ const CONFIG = {}
 CONFIG.OBJECTS_NUMBER = 100
 
 switch (location.origin) {
-    case 'https://bullseye.ctf.codeblue.jp':
-        CONFIG.API_BASE = '/external_api/v1/viewer'
+    case 'http://172.16.91.128:3000':
+        CONFIG.API_BASE = 'http://172.e72e7b2cfe3d888d7a03.codeblue.ohcihsiek.jp/external_api/v1/viewer'
+        break
+    case 'http://192.168.246.129:3000':
+        CONFIG.API_BASE = 'http://192.e72e7b2cfe3d888d7a03.codeblue.ohcihsiek.jp/external_api/v1/viewer'
+        //CONFIG.API_BASE = 'http://bullseye.demo.ohcihsiek.jp/api'
         break
     default:
-        CONFIG.API_BASE = 'http://e72e7b2cfe3d888d7a03f52f.codeblue.ohcihsiek.jp/external_api/v1/viewer'
-        //CONFIG.API_BASE = 'http://bullseye.demo.ohcihsiek.jp/api'
+        CONFIG.API_BASE = '/external_api/v1/viewer'
         break
 }
--- app/assets/javascripts/viewer/utils.js
+++ app/assets/javascripts/viewer/utils.js
@@ -1,37 +1,4 @@
 /**
  * bullseye viewer utils
  */
-
-// init ajax
-$.ajaxSetup({xhrFields: {withCredentials: true}, async: false});
-
-// util functions
-let _formatDate = function (start_at) {
-}
-
-function viewer_formatRound(problem_id, round_id) {
-    if (PROBLEMS[problem_id]['round'][round_id] === undefined) {
-        return `R-`
-    }
-
-    let start_date = new Date(Date.parse(PROBLEMS[problem_id]['round'][1]['start_at']))
-    let date = new Date(Date.parse(PROBLEMS[problem_id]['round'][round_id]['start_at']))
-    let day = (date.getDate() - start_date.getDate() + 1)
-    let hour = ('0' + date.getHours()).slice(-2)
-    let min = ('0' + date.getMinutes()).slice(-2)
-    return `R${round_id} (${day}day ${hour}:${min})`
-}
-
-// super globals
-window.PROBLEMS = {}
-$.getJSON(CONFIG.API_BASE + '/problems.json', (r) => {
-    window.PROBLEMS = r
-    for (let problem_id in r) {
-        let round = Object.keys(r[problem_id]['round'])
-        window.PROBLEMS[problem_id]['min_round'] = Math.min(...round)
-        window.PROBLEMS[problem_id]['max_round'] = Math.max(...round)
-    }
-})
-
-// finalize
-$.ajaxSetup({async: true});
+$.ajaxSetup({xhrFields: {withCredentials: true}});
